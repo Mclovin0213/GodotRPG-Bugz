3623500014f1693e4b3c8ef08dd3bbd4e558ad9b
Mclovin0213 <Javellaneda0213@gmail.com>
2022-08-06 14:54:48 -0700

    Add Interior of House2
    designed interior for the second house

--- UDEMY RPG/Scripts/House2.gd
+++ UDEMY RPG/Scripts/House2.gd
@@ -6,7 +6,7 @@ func _on_PlayerDetector_body_entered(body):
 	if body.name == "Player":
 		house.self_modulate.a = 0.5
 
-
 func _on_PlayerDetector_body_exited(body):
 	if body.name == "Player":
 		house.self_modulate.a = 1
+
--- UDEMY RPG/Scripts/House3.gd
+++ UDEMY RPG/Scripts/House3.gd
@@ -2,10 +2,8 @@ extends StaticBody2D
 
 func _on_Door_body_entered(body):
 	if body.name == "Player":
-		get_tree().change_scene("res://Prefabs/Interiors/InteriourHouse1.tscn")
-
+		get_tree().change_scene("res://Prefabs/Interiors/InteriorHouse2.tscn")
 
 func _on_SpawnPoint_body_entered(body):
 	if body.is_in_group("Player"):
 		Global.last_position = body.global_position
-	
