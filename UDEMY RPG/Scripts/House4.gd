8e9eefa885efb9ced45afe21d9a938ee2386e9ca
Mclovin0213 <javellaneda0213@gmail.com>
2022-08-08 18:46:55 -0700

    Add ShopHouse functionality

---
+++ UDEMY RPG/Scripts/House4.gd
@@ -0,0 +1,9 @@
+extends StaticBody2D
+
+func _on_Door_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene("res://Prefabs/Interiors/InteriorHouse3.tscn")
+
+func _on_SpawnPoint_body_entered(body):
+	if body.is_in_group("Player"):
+		Global.last_position = body.global_position
