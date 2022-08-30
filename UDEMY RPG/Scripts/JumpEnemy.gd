a7713f17b682b7733b03bf7f2bd671e3432b34cf
Mclovin0213 <Javellaneda0213@gmail.com>
2022-08-30 14:25:41 -0700

    Add jumping enemy

--- UDEMY RPG/Scripts/JumpEnemy.gd
+++ UDEMY RPG/Scripts/JumpEnemy.gd
@@ -3,11 +3,27 @@ extends KinematicBody2D
 onready var deathFX = preload("res://Prefabs/Effects/deathEffect.tscn")
 onready var anim = $anim
 
-var length = Vector2(50,50)
+var length = Vector2()
 export(float) var speed = 1
 
+var length_array = [
+	Vector2(0,50), 
+	Vector2(50,50), 
+	Vector2(0,-50), 
+	Vector2(-50,50), 
+	Vector2(-50,0), 
+	Vector2(-50,-50), 
+	Vector2(50, -50), 
+	Vector2(50,0)]
 var knockback = Vector2.ZERO
 var health = 2
+var player = null
+
+var state = IDLE
+enum {
+	IDLE,
+	ATTACK
+}
 
 var rng = RandomNumberGenerator.new()
 
@@ -19,6 +35,22 @@ func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 140 * delta)
 	knockback = move_and_slide(knockback)
 	
+func length_check():
+	if player != null:
+		length = player_length()
+	if player == null:
+		length = rand_length()
+		
+func rand_length():
+	var random_length = length_array[randi() % length_array.size()]
+	return random_length
+
+func player_length():
+	var p_length = Vector2()
+	var direction = (player.global_position - global_position)
+	p_length = direction
+	return p_length
+
 func move():
 	$Tween.interpolate_property(
 		self,
@@ -46,8 +78,12 @@ func _on_hitbox_area_entered(area):
 			queue_free()
 
 func _on_playerDetector_body_entered(body):
-	pass # Replace with function body.
+	if body.name == "Player":
+		player = body
 
 func _on_playerDetector_body_exited(body):
-	pass # Replace with function body.
+	if body.name == "Player":
+		player = null
 
+func _on_Tween_tween_started(object, key):
+	$audio.play()
